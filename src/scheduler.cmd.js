7938aba291a46dc6b9507df019d1ee5e3fd804d9
Draggor <draggor@gmail.com>
2013-10-25 00:19:48 -0500

    Restricting where you can drag stuff

--- src/scheduler.cmd.js
+++ src/scheduler.cmd.js
@@ -36,12 +36,23 @@ CMD.edit = function(id, name, time, host) {
 		}
 	}
 	e.html(data.name).data(data).css(css);
+	var revert = false;
 	e.draggable({
 		grid: [80,20]
 	,	appendTo: 'body'
-	,	helper: function() { return $('<div>').addClass('border-label').html(data.name).css(css).css({background:'gray'}); }
+	,	revert: false
+	,	helper: function() {
+			return $('<div>').addClass('border-label').html(data.name).css(css).css({background:'gray'});
+		}
+	,	start: function(event, ui) {
+			ui.helper.data('original-position', ui.helper.offset());
+		}
 	,	stop: function(event, ui) {
-			send(['move', [$(event.target).attr('id'), ui.position.top, ui.position.left]]);
+			if(ui.position.top < 68 || ui.position.left < 168) {
+				ui.helper.offset(ui.helper.data('original-position'));
+			} else {
+				send(['move', [$(event.target).attr('id'), ui.position.top, ui.position.left]]);
+			}
 		}
 	});
 	store(e);
